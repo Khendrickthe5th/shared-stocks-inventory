2931dcbed7992cef0176a7f6cdc50feb3b9d39fb
Ozoani Chidubem <flyinglego153971@gmail.com>
2026-03-19 07:56:36 +0100

    Initial Commit: Add core index file

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ *Plugin Name:       Shared Stocks Inventory
+ *Plugin URI:        https://example.com/plugins/the-basics/
+ *Description:       Lets variable products share stock quantity from a single stocks pool
+ *Version:           1.0.0
+ *Author:            Kris Dev
+ *License:           GPL v2 or later
+ *License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ *Update URI:        https://example.com/my-plugin/
+ */
+
+
+function echo_new_message(){
+	echo 'Birth of new Wordpress Plugin';
+                                   
+};
+add_action('init', 'echo_new_message');
+
+
